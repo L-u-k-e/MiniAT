5629ba20ff3173ae29f209b4f9c00bd28a90217e
L-u-k-e <parzycl1@sunyit.edu>
2016-02-03 20:56:52 -0500

    clean up fetch

--- vm/src/miniat_fetch.c
+++ vm/src/miniat_fetch.c
@@ -18,37 +18,32 @@
 void m_fetch_stage(miniat *m) {
 	
 	m_uword pc;
-	m_uword NPC;
-	m_memory_characteristics valid;
+	m_uword npc;
+	m_memory_characteristics addr_info;
 	m_uword data[M_INSTRUCTION_SIZE_WORDS];
 	m_uword int_tripped = 0;
 
 	pc = m_pc_remove_first(m);
-	NPC = pc + M_INSTRUCTION_SIZE_WORDS;
+	npc = pc + M_INSTRUCTION_SIZE_WORDS;
 
 	/*Check for a bad address*/
-	valid = m_memory_address_lookup(pc);
+	addr_info = m_memory_address_lookup(npc);
 
-	/*always push NPC if the queue is empty, the PC must get set to something no matter what!*/
+	/*
+	 * If the PC queue is empty, no deeper stages are trying to branch so its ok to add the NPC to the PC queue. Else, someone else is
+	 * branching and my NPC doesn't matter becuase it is past the delay slot (your the tail instruction in the delay slot)
+	 */
 	if(m->pc.size == 0) {
-		m_pc_add_last(m, NPC);
-
+		m_pc_add_last(m, npc);
 	}
 
-	/*if I am valid complete the fetch*/
-	if((valid.type != m_memory_type_invalid) && (valid.type == m_memory_type_flash || valid.type == m_memory_type_ram)) {
-
-
-		/*
-		 * If the PC queue is empty, no deeper stages are trying to branch so its ok to add the NPC to the PC queue. Else, someone else is
-		 * branching and my NPC doesn't matter becuase it is past the delay slot (your the tail instruction in the delay slot)
-		 */
+	/*Complete the fetch so long as the NPC is in RAM or Flash*/
+	if((addr_info.type == m_memory_type_flash || addr_info.type == m_memory_type_ram)) {
 		m_memory_read(m, pc, &data[M_LOW]);
 		m_memory_read(m, pc+1, &data[M_HIGH]);
 	}
-	/*illegal address*/
 	else {
-		/*trip the ifr for illegal address */
+		/*Trip the ifr for illegal address */
 		int_tripped = M_INT_ILLEGAL_ADDRESS;
 	}
 
@@ -62,7 +57,7 @@ void m_fetch_stage(miniat *m) {
 	else {
 		m_pipeline_morph_to_INT(m, m_pipeline_stage_fetch, int_tripped);
 	}
-	m->pipeline.fetch.out.NPC = NPC;
+	m->pipeline.fetch.out.NPC = npc;
 
 	return;
 }
